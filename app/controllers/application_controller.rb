3f4258cad42a8693e5e45876ee1e6862787fca73
Renan Bona <rcbp3094@gmail.com>
2019-12-13 23:45:00 -0300

    Generate devise views and add style to them

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_devise_params, if: :devise_controller?
+
+  def configure_devise_params
+    devise_parameter_sanitizer.permit(
+      :sign_up,
+      keys: %i[name email password password_confirmation]
+    )
+  end
 end
