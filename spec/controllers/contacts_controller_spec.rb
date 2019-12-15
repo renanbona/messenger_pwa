dfecffda5437e0ad77d20ecd7a45209beb82f060
Renan Bona <rcbp3094@gmail.com>
2019-12-15 16:25:31 -0300

    Change home and contacts controller

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe ContactsController, type: :controller do
+  describe 'GET #index' do
+    context 'when user is not logged in' do
+      it 'redirects to log in page' do
+        get :index
 
+        expect(response).to have_http_status(302)
+      end
+    end
+
+    context 'when user is logged in' do
+      let(:user) { create(:user) }
+
+      it 'returns a successfull response' do
+        sign_in user
+        get :index
+
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
   describe "GET #index" do
+    let(:user) { create(:user) }
+
+    before { sign_in user }
+
     it "returns http success" do
-      get :index
+      get :index, params: {}
 
       expect(response).to have_http_status(:success)
     end
